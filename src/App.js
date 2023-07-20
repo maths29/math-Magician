baed60f8c54feca3ef64772bb721a2a5a4d507fc
mathsoft29 <iwuconcept@gmail.com>
2023-07-20 18:53:13 +0100

    Added displayQuote component with API integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/calculator';
+import Quote from './components/displayQuote';
 
 function App() {
   return (
     <div className="Container">
       <Calculator />
+      <Quote />
     </div>
   );
 }
