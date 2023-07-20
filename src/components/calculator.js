ddbd2b767b5fbd69466a6d9f42be8598c9157c23
mathsoft29 <iwuconcept@gmail.com>
2023-07-20 14:32:12 +0100

    implement click event handle for calculator's buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,39 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './addButton';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    <div className="input">0</div>
+  function handleClick(buttonName) {
+    const newState = calculate(state, buttonName);
+    setState(newState);
+  }
 
-    <div className="buttonContainer">
-      <Button label="AC" />
-      <Button label="+/-" />
-      <Button label="%" />
-      <Button label="÷" className="operation" />
+  return (
+    <div className="calculator">
+      <div className="buttonContainer" />
+      <div className="input">{state.next || state.total || '0'}</div>
 
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Button label="X" className="operation" />
+      <div className="buttonContainer">
+        <Button label="AC" onClick={() => handleClick('AC')} />
+        <Button label="+/-" onClick={() => handleClick('+/-')} />
+        <Button label="%" onClick={() => handleClick('%')} />
+        <Button label="÷" className="operation" onClick={() => handleClick('÷')} />
 
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Button label="-" className="operation" />
+        <Button label="7" onClick={() => handleClick('7')} />
+        <Button label="8" onClick={() => handleClick('8')} />
+        <Button label="9" onClick={() => handleClick('9')} />
+        <Button label="x" className="operation" onClick={() => handleClick('x')} />
 
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Button label="+" className="operation" />
+        <Button label="4" onClick={() => handleClick('4')} />
+        <Button label="5" onClick={() => handleClick('5')} />
+        <Button label="6" onClick={() => handleClick('6')} />
+        <Button label="-" className="operation" onClick={() => handleClick('-')} />
 
-      <Button label="0" className="zero" />
-      <Button label="." />
-      <Button label="=" className="operation" />
+        <Button label="1" onClick={() => handleClick('1')} />
+        <Button label="2" onClick={() => handleClick('2')} />
+        <Button label="3" onClick={() => handleClick('3')} />
+        <Button label="+" className="operation" onClick={() => handleClick('+')} />
+
+        <Button label="0" className="zero" onClick={() => handleClick('0')} />
+        <Button label="." onClick={() => handleClick('.')} />
+        <Button label="=" className="operation" onClick={() => handleClick('=')} />
+      </div>
+
     </div>
-
-  </div>
-
-);
+  );
+};
 
 export default Calculator;
